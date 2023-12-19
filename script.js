c1975da968b6dcfa960d82b95bf162bb5f34bef2
ByteBigBoss <nethminas42@gmail.com>
2023-12-19 19:17:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const changeView = ()=>{
+  var signUpBox = document.getElementById("signUpBox");
+  var signInBox = document.getElementById("signInBox");
+  
+  signInBox.classList.toggle('d-none');
+  signUpBox.classList.toggle('d-none');
+};
